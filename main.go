bd5c38b196440f593f9ba38175d2845fc9e3f008
Jim Browne <jbrowne@jbrowne.com>
2022-12-04 00:25:37 -0800

    Fold "_indoor" API fields into non-suffixed fields
    Per the API documentation:
    
        A user can also designate each device as either indoor or
        outdoor. All indoor observation value fields will end with an
        _indoor suffix. Outdoor observations fields do not have a suffix.
    
    This seems odd to me, but it is what it is.  So if a uesr designates a
    station as "indoor" we will now fold those values into the non
    "indoor" fields.

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"reflect"
 	"strconv"
 	"time"
 
@@ -48,41 +49,59 @@ type stationStatus struct {
 
 // observation is the typed observation data from a station
 type observation struct {
-	AirDensity                       float64 `json:"air_density"`
-	AirTemperature                   float64 `json:"air_temperature"`
-	BarometricPressure               float64 `json:"barometric_pressure"`
-	Brightness                       float64 `json:"brightness"`
-	DeltaT                           float64 `json:"delta_t"`
-	DewPoint                         float64 `json:"dew_point"`
-	FeelsLike                        float64 `json:"feels_like"`
-	HeatIndex                        float64 `json:"heat_index"`
-	LightningStrikeCount             float64 `json:"lightning_strike_count"`
-	LightningStrikeCountLast1hr      float64 `json:"lightning_strike_count_last_1hr"`
-	LightningStrikeCountLast3hr      float64 `json:"lightning_strike_count_last_3hr"`
-	LightningStrikeLastDistance      float64 `json:"lightning_strike_last_distance"`
-	LightningStrikeLastEpoch         float64 `json:"lightning_strike_last_epoch"`
-	Precip                           float64 `json:"precip"`
-	PrecipAccumLast1hr               float64 `json:"precip_accum_last_1hr"`
-	PrecipAccumLocalDay              float64 `json:"precip_accum_local_day"`
-	PrecipAccumLocalYesterday        float64 `json:"precip_accum_local_yesterday"`
-	PrecipAccumLocalYesterdayFinal   float64 `json:"precip_accum_local_yesterday_final"`
-	PrecipAnalysisTypeYesterday      float64 `json:"precip_analysis_type_yesterday"`
-	PrecipMinutesLocalDay            float64 `json:"precip_minutes_local_day"`
-	PrecipMinutesLocalYesterday      float64 `json:"precip_minutes_local_yesterday"`
-	PrecipMinutesLocalYesterdayFinal float64 `json:"precip_minutes_local_yesterday_final"`
-	PressureTrend                    string  `json:"pressure_trend"`
-	RelativeHumidity                 float64 `json:"relative_humidity"`
-	SeaLevelPressure                 float64 `json:"sea_level_pressure"`
-	SolarRadiation                   float64 `json:"solar_radiation"`
-	StationPressure                  float64 `json:"station_pressure"`
-	Timestamp                        float64 `json:"timestamp"`
-	Uv                               float64 `json:"uv"`
-	WetBulbTemperature               float64 `json:"wet_bulb_temperature"`
-	WindAvg                          float64 `json:"wind_avg"`
-	WindChill                        float64 `json:"wind_chill"`
-	WindDirection                    float64 `json:"wind_direction"`
-	WindGust                         float64 `json:"wind_gust"`
-	WindLull                         float64 `json:"wind_lull"`
+	AirDensity                        float64 `json:"air_density"`
+	AirDensityIndoor                  float64 `json:"air_density_indoor"`
+	AirTemperature                    float64 `json:"air_temperature"`
+	AirTemperatureIndoor              float64 `json:"air_temperature_indoor"`
+	BarometricPressure                float64 `json:"barometric_pressure"`
+	BarometricPressureIndoor          float64 `json:"barometric_pressure_indoor"`
+	Brightness                        float64 `json:"brightness"`
+	DeltaT                            float64 `json:"delta_t"`
+	DeltaTIndoor                      float64 `json:"delta_t_indoor"`
+	DewPoint                          float64 `json:"dew_point"`
+	DewPointIndoor                    float64 `json:"dew_point_indoor"`
+	FeelsLike                         float64 `json:"feels_like"`
+	FeelsLikeIndoor                   float64 `json:"feels_like_indoor"`
+	HeatIndex                         float64 `json:"heat_index"`
+	HeatIndexIndoor                   float64 `json:"heat_index_indoor"`
+	LightningStrikeCount              float64 `json:"lightning_strike_count"`
+	LightningStrikeCountIndoor        float64 `json:"lightning_strike_count_indoor"`
+	LightningStrikeCountLast1hr       float64 `json:"lightning_strike_count_last_1hr"`
+	LightningStrikeCountLast1hrIndoor float64 `json:"lightning_strike_count_last_1hr_indoor"`
+	LightningStrikeCountLast3hr       float64 `json:"lightning_strike_count_last_3hr"`
+	LightningStrikeCountLast3hrIndoor float64 `json:"lightning_strike_count_last_3hr_indoor"`
+	LightningStrikeLastDistance       float64 `json:"lightning_strike_last_distance"`
+	LightningStrikeLastDistanceIndoor float64 `json:"lightning_strike_last_distance_indoor"`
+	LightningStrikeLastEpoch          float64 `json:"lightning_strike_last_epoch"`
+	LightningStrikeLastEpochIndoor    float64 `json:"lightning_strike_last_epoch_indoor"`
+	Precip                            float64 `json:"precip"`
+	PrecipAccumLast1hr                float64 `json:"precip_accum_last_1hr"`
+	PrecipAccumLocalDay               float64 `json:"precip_accum_local_day"`
+	PrecipAccumLocalYesterday         float64 `json:"precip_accum_local_yesterday"`
+	PrecipAccumLocalYesterdayFinal    float64 `json:"precip_accum_local_yesterday_final"`
+	PrecipAnalysisTypeYesterday       float64 `json:"precip_analysis_type_yesterday"`
+	PrecipMinutesLocalDay             float64 `json:"precip_minutes_local_day"`
+	PrecipMinutesLocalYesterday       float64 `json:"precip_minutes_local_yesterday"`
+	PrecipMinutesLocalYesterdayFinal  float64 `json:"precip_minutes_local_yesterday_final"`
+	PressureTrend                     string  `json:"pressure_trend"`
+	PressureTrendIndoor               string  `json:"pressure_trend_indoor"`
+	RelativeHumidity                  float64 `json:"relative_humidity"`
+	RelativeHumidityIndoor            float64 `json:"relative_humidity_indoor"`
+	SeaLevelPressure                  float64 `json:"sea_level_pressure"`
+	SeaLevelPressureIndoor            float64 `json:"sea_level_pressure_indoor"`
+	SolarRadiation                    float64 `json:"solar_radiation"`
+	StationPressure                   float64 `json:"station_pressure"`
+	StationPressureIndoor             float64 `json:"station_pressure_indoor"`
+	Timestamp                         float64 `json:"timestamp"`
+	Uv                                float64 `json:"uv"`
+	WetBulbTemperature                float64 `json:"wet_bulb_temperature"`
+	WetBulbTemperatureIndoor          float64 `json:"wet_bulb_temperature_indoor"`
+	WindAvg                           float64 `json:"wind_avg"`
+	WindChill                         float64 `json:"wind_chill"`
+	WindChillIndoor                   float64 `json:"wind_chill_indoor"`
+	WindDirection                     float64 `json:"wind_direction"`
+	WindGust                          float64 `json:"wind_gust"`
+	WindLull                          float64 `json:"wind_lull"`
 }
 
 // response is our response from the weatherflow obvservations API
@@ -96,6 +115,22 @@ type response struct {
 	Elevation   float64       `json:"elevation"`
 	Status      stationStatus `json:"status"`
 	Obs         []observation `json:"obs"`
+}
+
+func mapIndoor(obs *observation) {
+	v := reflect.ValueOf(obs)
+	typeOfObs := v.Elem().Type()
+
+	for i := 0; i < v.Elem().NumField(); i++ {
+		fieldName := typeOfObs.Field(i).Name
+		// fmt.Printf("Checking field %s\n", fieldName)
+		indoorName := fieldName + "Indoor"
+		indoorValue := v.Elem().FieldByName(indoorName)
+		if indoorValue.IsValid() && !indoorValue.IsZero() {
+			// fmt.Printf("Field: %s has an Indoor and its value is %v\n", fieldName, indoorValue)
+			v.Elem().FieldByName(fieldName).Set(indoorValue)
+		}
+	}
 }
 
 // getTempestData retrieves the API response from our Tempest weather station
@@ -139,6 +174,7 @@ func getDatas() {
 		labels = r.parseLabels()
 		if len(r.Obs) > 0 {
 			o := r.Obs[0]
+			mapIndoor(&o)
 			metrics.SetAll(o, labels)
 		}
 		time.Sleep(time.Second * 15)
